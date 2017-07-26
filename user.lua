3e001293c8df4b5d7bbabad48d7bf6a0ce1eacd9
Ryan Gooler <ryan.gooler@gmail.com>
2017-07-26 02:48:09 -0700

    Better init

--- user.lua
+++ user.lua
@@ -4,9 +4,6 @@ dhcp_config ={start = "192.168.1.100"}
 wifi.ap.dhcp.config(dhcp_config)
 
 mdns.register("fileserver", { description="Jippen's esp8266 File Server", service="http", port=80, location='Jippen' })
+dofile('OLED_dcfur_on.lua')
 dofile('OLED_loop.lua')
-if file.exists("http-init.lc") then
-   dofile("http-init.lc")
-else
-   dofile("http-init.lua")
-end
+dofile('http-init.lua')
